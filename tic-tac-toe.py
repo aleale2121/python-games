24beae2e348be7c13ebbca0d10daeaa4b5133d62
alefew <alefewyimer4@gmail.com>
2017-01-03 13:30:35 +0300

    optimised code added

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,173 +1,101 @@
-import __future__
-#board
-freespace = ['_',' ']
-board = ['_','_','_','_','_','_',' ',' ',' '] 
-def current_board():
-	print('   a   b   c \n1 _%s_|_%s_|_%s_\n2 _%s_|_%s_|_%s_\n3  %s | %s | %s' % (board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8]))
-def player_start():
-	who = raw_input('Who will go first?: ')
-	if who.upper() == 'X':
-		current_board()
-		player_x()
-	elif who.upper() == 'O':
-		current_board()
-		player_o()
-	else:
-		print("You didn't type a X or an O, please enter again.")
-		player_start()
-#player X
-def player_x():
-	move = raw_input('Player X enter the space you want.: ')
-	if move == 'a1' and board[0] in freespace:
-		board[0] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'b1' and board[1] in freespace:
-		board[1] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'c1' and board[2] in freespace:
-		board[2] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'a2' and board[3] in freespace:
-		board[3] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'b2' and board[4] in freespace:
-		board[4] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'c2' and board[5] in freespace:
-		board[5] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'a3' and board[6] in freespace:
-		board[6] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'b3' and board[7] in freespace:
-		board[7] = 'X'
-		current_board()
-		winner()
-		player_o()
-	elif move == 'c3' and board[8] in freespace:
-		board[8] = 'X'
-		current_board()
-		winner()
-		player_o()
-	else:
-		print('Error, this space is taken or you entered an incorrect space.')
-		player_x()
-#player O
-def player_o():
-	move = raw_input('Player O enter the the space you want.: ')
-	if move == 'a1' and board[0] in freespace:
-		board[0] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'b1' and board[1] in freespace:
-		board[1] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'c1' and board[2] in freespace:
-		board[2] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'a2' and board[3] in freespace:
-		board[3] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'b2' and board[4] in freespace:
-		board[4] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'c2' and board[5] in freespace:
-		board[5] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'a3' and board[6] in freespace:
-		board[6] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'b3' and board[7] in freespace:
-		board[7] = 'O'
-		current_board()
-		winner()
-		player_x()
-	elif move == 'c3' and board[8] in freespace:
-		board[8] = 'O'
-		current_board()
-		winner()
-		player_x()
-	else:
-		print('Error, this space is taken or you entered an incorrect space.')
-		player_o()
-#checks for a winner		
-def winner():
-    if board[0] == 'X' and board[1] == 'X' and board[2] == 'X' or \
-       board[3] == 'X' and board[4] == 'X' and board[5] == 'X' or \
-       board[6] == 'X' and board[7] == 'X' and board[8] == 'X' or \
-       board[0] == 'X' and board[3] == 'X' and board[6] == 'X' or \
-       board[1] == 'X' and board[4] == 'X' and board[7] == 'X' or \
-       board[2] == 'X' and board[5] == 'X' and board[8] == 'X' or \
-       board[0] == 'X' and board[4] == 'X' and board[8] == 'X' or \
-       board[2] == 'X' and board[4] == 'X' and board[6] == 'X':
-        current_board()
-        print('Winner is X')
-        play_again()
-    elif board[0] == 'O' and board[1] == 'O' and board[2] == 'O' or \
-         board[3] == 'O' and board[4] == 'O' and board[5] == 'O' or \
-         board[6] == 'O' and board[7] == 'O' and board[8] == 'O' or \
-         board[0] == 'O' and board[3] == 'O' and board[6] == 'O' or \
-         board[1] == 'O' and board[4] == 'O' and board[7] == 'O' or \
-         board[2] == 'O' and board[5] == 'O' and board[8] == 'O' or \
-         board[0] == 'O' and board[4] == 'O' and board[8] == 'O' or \
-         board[2] == 'O' and board[4] == 'O' and board[6] == 'O':
-        current_board()
-        print('Winner is O')
-        play_again()
-    elif board[0] != '_' and board[1] != '_' and board[2] != '_' and \
-	     board[3] != '_' and board[4] != '_' and board[5] != '_' and \
-		 board[6] != ' ' and board[7] != ' ' and board[8] != ' ':
-        current_board()
-        print('This game is a draw.')
-        play_again()
-#resets the board after every game		
+from __future__ import print_function
+
+# Constants
+EMPTY = '_'
+SPACE = ' '
+PLAYER_X = 'X'
+PLAYER_O = 'O'
+BOARD_SIZE = 9
+
+# Initialize the board
+board = [EMPTY] * 6 + [SPACE] * 3
+
+# Mapping of moves to board indices
+move_to_index = {
+    'a1': 0, 'b1': 1, 'c1': 2,
+    'a2': 3, 'b2': 4, 'c2': 5,
+    'a3': 6, 'b3': 7, 'c3': 8
+}
+
+# Winning combinations
+winning_combinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+    [0, 4, 8], [2, 4, 6]              # Diagonals
+]
+
+def print_board():
+    """Prints the current state of the board."""
+    print('   a   b   c')
+    print(f'1 _{board[0]}_|_{board[1]}_|_{board[2]}_')
+    print(f'2 _{board[3]}_|_{board[4]}_|_{board[5]}_')
+    print(f'3  {board[6]} | {board[7]} | {board[8]}')
+
+def is_valid_move(move):
+    """Checks if the move is valid."""
+    return move in move_to_index and board[move_to_index[move]] in (EMPTY, SPACE)
+
+def make_move(move, player):
+    """Updates the board with the player's move."""
+    board[move_to_index[move]] = player
+
+def check_winner(player):
+    """Checks if the player has won."""
+    for combo in winning_combinations:
+        if all(board[i] == player for i in combo):
+            return True
+    return False
+
+def is_board_full():
+    """Checks if the board is full."""
+    return all(cell != EMPTY and cell != SPACE for cell in board)
+
 def reset_board():
-	board[0] = '_'
-	board[1] = '_'
-	board[2] = '_'
-	board[3] = '_'
-	board[4] = '_'
-	board[5] = '_'
-	board[6] = ' '
-	board[7] = ' '
-	board[8] = ' '
-#asks if you want to play again	and resets the game
+    """Resets the board to its initial state."""
+    global board
+    board = [EMPTY] * 6 + [SPACE] * 3
+
+def player_turn(player):
+    """Handles a player's turn."""
+    print_board()
+    move = input(f'Player {player}, enter the space you want (e.g., a1): ').strip().lower()
+    if is_valid_move(move):
+        make_move(move, player)
+        if check_winner(player):
+            print_board()
+            print(f'Winner is {player}!')
+            play_again()
+        elif is_board_full():
+            print_board()
+            print('This game is a draw.')
+            play_again()
+        else:
+            player_turn(PLAYER_O if player == PLAYER_X else PLAYER_X)
+    else:
+        print('Error: Invalid move. Please try again.')
+        player_turn(player)
+
 def play_again():
-	playagain = raw_input('Would you like to play again? Enter Y or N: ')
-	
-	if playagain.upper() == 'Y':
-		reset_board()
-		player_start()
-	elif playagain.upper() == 'N':
-		quit()
-	else:
-		print("You didn't type Y on N, please try again.")
-		play_again()
-player_start()
+    """Asks the players if they want to play again."""
+    choice = input('Would you like to play again? Enter Y or N: ').strip().upper()
+    if choice == 'Y':
+        reset_board()
+        start_game()
+    elif choice == 'N':
+        print('Thanks for playing!')
+        exit()
+    else:
+        print('Invalid choice. Please enter Y or N.')
+        play_again()
+
+def start_game():
+    """Starts the game."""
+    first_player = input('Who will go first? (X or O): ').strip().upper()
+    if first_player in (PLAYER_X, PLAYER_O):
+        player_turn(first_player)
+    else:
+        print('Invalid choice. Please enter X or O.')
+        start_game()
+
+# Start the game
+start_game()
